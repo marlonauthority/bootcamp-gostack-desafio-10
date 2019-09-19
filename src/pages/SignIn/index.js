ffc8e3393857f664ebb62932719b402767aba480
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-19 15:51:30 -0400

    Component Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,12 +4,14 @@ import { Text, SafeAreaView } from 'react-native';
 // import { Container } from './styles';
 import Background from '~/components/Background';
 import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 export default function SignIn() {
   return (
     <Background>
       <SafeAreaView>
         <Input placeholder="Digite seu e-mail" />
+        <Button>Entrar</Button>
       </SafeAreaView>
     </Background>
   );
