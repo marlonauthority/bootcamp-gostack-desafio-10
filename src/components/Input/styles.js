6a8a70a8cf5a462f631dd3b9ce7fe34872f2162c
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-19 16:12:08 -0400

    Ajuste no padding do placeholder Input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   height: 50px;
-  padding: 0 15px;
+
   background: rgba(0, 0, 0, 0.1);
   border-radius: 4px;
   flex-direction: row;
@@ -14,6 +14,6 @@ export const TInput = styled.TextInput.attrs({
 })`
   flex: 1;
   font-size: 18px;
-  margin-left: 20px;
+  padding: 14px 20px;
   color: #fff;
 `;
